f387ceab24286aa139a2824b815f94eb8de0144d
aaochoa <aaochoa4@gmail.com>
2020-07-28 22:04:52 -0500

    Completing user auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,38 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const {registerValidation, loginValidation} = require('../validation')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const {registerValidation, loginValidation} = require('../validation');
 
 router.post('/register', async (req, res) => {
   const {error} = registerValidation(req.body);
-  if (error) return res.status(400).send(error);
+  if (error) return res.status(400).send(error.details[0].message);
+  const emailExist = await User.findOne({email: req.body.email});
+  if (emailExist) return res.status(400).send('Email aready exists.');
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
   try {
     const savedUser = await user.save();
-    console.log(savedUser);
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch(err) {
     res.status(400).send(err);
   }
 });
+
+router.post('/login', async (req, res) => {
+  const {error} = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const user = await User.findOne({email: req.body.email});
+  if (!user) return res.status(400).send('Email or password is wrong.');
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Email or password is wrong.');
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth-token', token, {expiresIn: '1h'}).send(token);
+})
 
 module.exports = router;
