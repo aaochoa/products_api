ca64baa134a977e06f3558cfdf166e0b2009b06c
aaochoa <aaochoa4@gmail.com>
2020-07-29 07:36:28 -0500

    Deprecated method mongoose

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
+const productsRoute = require('./routes/products');
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to db'));
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to db'));
 
 app.use(express.json());
 
